87e3dd99ccec13e004641f9dc230a55c91a28d94
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-04 19:42:07 +0200

    Update stg_bkpf.sql

--- models/stg/stg_bkpf.sql
+++ models/stg/stg_bkpf.sql
@@ -1,14 +1,10 @@
 
 {{ config(materialized='table', schema=env_var('DBT_ENV') ~ '_STG') }}
 
-with source_data as (
-    select $1 as data
-    from @s3_stage/path/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/bkpf.json
-)
-
 select
-    parse_json(data):bukrs::string as company_code,
-    parse_json(data):belnr::string as document_number,
-    parse_json(data):gjahr::string as fiscal_year,
-    parse_json(data):blart::string as doc_type
-from source_data;
+    $1::string as bukrs,
+    $2::string as belnr,
+    $3::string as gjahr,
+    $4::string as blart
+from @DEV_SAP_STG.STG.SAP_STAGE/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/bkpf.csv;
+
