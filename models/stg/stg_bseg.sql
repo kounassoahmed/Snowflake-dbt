d086b5b9aae0f4490efcfa98a4c670335052212c
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-04 19:42:33 +0200

    Update stg_bseg.sql

--- models/stg/stg_bseg.sql
+++ models/stg/stg_bseg.sql
@@ -1,15 +1,9 @@
-
 {{ config(materialized='table', schema=env_var('DBT_ENV') ~ '_STG') }}
 
-with source_data as (
-    select $1 as data
-    from @s3_stage/path/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/bseg.json
-)
-
 select
-    parse_json(data):bukrs::string as company_code,
-    parse_json(data):belnr::string as document_number,
-    parse_json(data):gjahr::string as fiscal_year,
-    parse_json(data):buzei::string as line_item,
-    parse_json(data):wrbtr::float as amount
-from source_data;
+  $1::string as bukrs,
+  $2::string as belnr,
+  $3::string as gjahr,
+  $4::string as buzei,
+  $5::float as wrbtr
+from @DEV_SAP_STG.STG.SAP_STAGE/2024/06/01/bseg.csv;
