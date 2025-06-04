453f69a5794537710f5c05ac17433e009936916b
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-04 19:42:47 +0200

    Update stg_t001.sql

--- models/stg/stg_t001.sql
+++ models/stg/stg_t001.sql
@@ -1,13 +1,7 @@
-
 {{ config(materialized='table', schema=env_var('DBT_ENV') ~ '_STG') }}
 
-with source_data as (
-    select $1 as data
-    from @s3_stage/path/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/t001.json
-)
-
 select
-    parse_json(data):bukrs::string as company_code,
-    parse_json(data):butxt::string as company_name,
-    parse_json(data):land1::string as country
-from source_data;
+  $1::string as bukrs,
+  $2::string as butxt,
+  $3::string as land1
+from @DEV_SAP_STG.STG.SAP_STAGE/2024/06/01/t001.csv;
