99405000975cf5a69fa8742e94fd4566978e49a4
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-05 15:32:11 +0200

    Update t001.sql

--- models/stg/t001.sql
+++ models/stg/t001.sql
@@ -3,4 +3,4 @@ select
   $1::string as bukrs,
   $2::string as butxt,
   $3::string as land1
-from @DEV_SAP_STG.STG.SAP_STAGE/2024/06/01/t001.csv;
+from @DEV_SAP_STG.STG.SAP_STAGE/2024/06/01/t001.csv
