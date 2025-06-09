8e87ae39e7911a7b641f7e6d7fba2a2a0d1633b2
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-10 00:06:44 +0200

    Update stg_bkpf.sql

--- models/stg/stg_bkpf.sql
+++ models/stg/stg_bkpf.sql
@@ -1,8 +1,9 @@
 {{ config(materialized='table', schema='STG') }}
 
 select
-  $1::string as bukrs,
-  $2::string as butxt,
-  $3::string as land1
+    $1::string as bukrs,
+    $2::string as belnr,
+    $3::string as gjahr,
+    $4::string as blart
 from @DEV_SAP_STG.STG.SAP_STAGE/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/bkpf.csv
 (file_format => DEV_SAP_STG.STG.CSV_FORMAT)
