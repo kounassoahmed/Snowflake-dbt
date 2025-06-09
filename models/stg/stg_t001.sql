4e534e5f8c88da0236ad2eaf0b405828c217179a
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-09 23:50:54 +0200

    Update stg_t001.sql

--- models/stg/stg_t001.sql
+++ models/stg/stg_t001.sql
@@ -4,4 +4,5 @@ select
   $1::string as bukrs,
   $2::string as butxt,
   $3::string as land1
-from @DEV_SAP_STG.STG.SAP_STAGE/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/t001.csv;
+from @DEV_SAP_STG.STG.SAP_STAGE/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/t001.csv
+(file_format => DEV_SAP_STG.STG.CSV_FORMAT);
