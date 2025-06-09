c5013f3693beecf8e57b76dee7833dbcfea872ba
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-10 00:34:52 +0200

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,15 +1,9 @@
-
 {% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema | lower -%}
 
-    {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
-
         {{ default_schema }}
-
     {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
+        {{ custom_schema_name | lower }}
     {%- endif -%}
-
 {%- endmacro %}
