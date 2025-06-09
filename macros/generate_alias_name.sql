607caf58ac634078f6bcca325cf7a51eeadf388b
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-09 22:43:10 +0200

    Update generate_alias_name.sql

--- macros/generate_alias_name.sql
+++ macros/generate_alias_name.sql
@@ -1,22 +1,3 @@
-{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
-    {%- set raw_name = node.name -%}
-
-    {# Supprimer les préfixes raw_, stg_ ou dwh_ #}
-    {%- set cleaned_name = (
-        raw_name
-        | regex_replace('^stg_', '')
-        | regex_replace('^raw_', '')
-        | regex_replace('^dwh_', '')
-    ) -%}
-
-    {%- if custom_alias_name -%}
-        {{ custom_alias_name | trim }}
-
-    {%- elif node.version -%}
-        {{ cleaned_name ~ "_v" ~ (node.version | replace(".", "_")) }}
-
-    {%- else -%}
-        {{ cleaned_name }}
-
-    {%- endif -%}
-{%- endmacro %}
+{% macro generate_alias_name(custom_alias_name=none, node=none) %}
+    test_macro
+{% endmacro %}
