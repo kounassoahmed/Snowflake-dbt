9310b097dfc0e725882ad48b191c3ad7a3099096
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-10 00:06:02 +0200

    Update stg_bseg.sql

--- models/stg/stg_bseg.sql
+++ models/stg/stg_bseg.sql
@@ -2,7 +2,9 @@
 
 select
   $1::string as bukrs,
-  $2::string as butxt,
-  $3::string as land1
+  $2::string as belnr,
+  $3::string as gjahr,
+  $4::string as buzei,
+  $5::float as wrbtr
 from @DEV_SAP_STG.STG.SAP_STAGE/{{ var('year') }}/{{ var('month') }}/{{ var('day') }}/bseg.csv
 (file_format => DEV_SAP_STG.STG.CSV_FORMAT)
