20c392a598c0bd98ec81038e0bd3f7900f3b1a27
kounassoahmed <kounassoahmed.pro@gmail.com>
2025-06-10 10:32:44 +0200

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,14 +1,14 @@
-{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
-  {%- set raw_name = node.name -%}
-  {%- set cleaned_name = raw_name
-    | replace('stg_', '')
-    | replace('raw_', '')
-    | replace('dwh_', '')
-  -%}
+{% macro generate_schema_name(custom_schema_name, node) -%}
 
-  {%- if custom_alias_name -%}
-    {{ custom_alias_name | trim }}
-  {%- else -%}
-    {{ cleaned_name }}
-  {%- endif -%}
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
 {%- endmacro %}
